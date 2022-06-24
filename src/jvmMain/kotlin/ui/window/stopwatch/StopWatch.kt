297b225cb3bab387778fc430842303e30394a926
Gamaliel Garcia <46827955+EGAMAGZ@users.noreply.github.com>
2022-06-24 18:17:44 -0500

    Creation of classes for stopwatch

---
+++ src/jvmMain/kotlin/ui/window/stopwatch/StopWatch.kt
@@ -0,0 +1,4 @@
+package ui.window.stopwatch
+
+class StopWatch {
+}
---
+++ src/jvmMain/kotlin/ui/window/stopwatch/StopWatchScreen.kt
@@ -0,0 +1,10 @@
+package ui.window.stopwatch
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import navigation.NavController
+
+@Composable
+fun StopWatchScreen(navController: NavController, modifier: Modifier = Modifier) {
+    
+}
