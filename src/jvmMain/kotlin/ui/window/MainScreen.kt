cc5785febb62f85872ca3ee4a74a048bcf1c309d
Gamaliel Garcia <46827955+EGAMAGZ@users.noreply.github.com>
2022-06-24 19:08:55 -0500

    Simple StopWatch

--- src/jvmMain/kotlin/ui/window/MainScreen.kt
+++ src/jvmMain/kotlin/ui/window/MainScreen.kt
@@ -3,8 +3,9 @@ package ui.window
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import navigation.NavController
+import ui.window.stopwatch.StopWatchScreen
 
 @Composable
 fun MainScreen(navController: NavController) {
-    Text("Hello")
+    StopWatchScreen()
 }
--- src/jvmMain/kotlin/ui/window/stopwatch/StopWatchScreen.kt
+++ src/jvmMain/kotlin/ui/window/stopwatch/StopWatchScreen.kt
@@ -1,10 +1,69 @@
 package ui.window.stopwatch
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import navigation.NavController
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
-fun StopWatchScreen(navController: NavController, modifier: Modifier = Modifier) {
-    
+fun StopWatchScreen(modifier: Modifier = Modifier) {
+    val stopwatch = remember { StopWatch() }
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        StopWatchDisplay(
+            formattedTime = stopwatch.formattedTime,
+            onStartClick = stopwatch::start,
+            onPauseClick = stopwatch::pause,
+            onResetClick = stopwatch::reset
+        )
+    }
+}
+
+@Composable
+fun StopWatchDisplay(
+    formattedTime: String,
+    onStartClick: () -> Unit,
+    onPauseClick: () -> Unit,
+    onResetClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(
+            text = formattedTime,
+            fontWeight = FontWeight.Bold,
+            fontSize = 30.sp,
+            color = Color.Black
+        )
+        Spacer(Modifier.height(16.dp))
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Button(onStartClick) {
+                Text("Start")
+            }
+            Spacer(Modifier.width(16.dp))
+            Button(onPauseClick) {
+                Text("Pause")
+            }
+            Spacer(Modifier.width(16.dp))
+            Button(onResetClick) {
+                Text("Reset")
+            }
+        }
+    }
 }
